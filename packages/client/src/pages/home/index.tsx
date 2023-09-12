eb6331a0c5a91460c3fc507f1eaf7e937a8439c9
jovicheng <jovicheng@foxmail.com>
2023-09-12 21:04:51 +0800

    update assets url

--- packages/client/src/pages/home/index.tsx
+++ packages/client/src/pages/home/index.tsx
@@ -1,81 +1,124 @@
-import React, { useState } from 'react';
-import './styles.scss';
-import { Link } from 'react-router-dom';
-import { InputNumber, Popover, Slider } from 'antd';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import React, { useState } from "react";
+import "./styles.scss";
+import { Link } from "react-router-dom";
+import { InputNumber, Popover, Slider } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
+
+import earthImg from "../../assets/earth.png";
+import grassImg from "../../assets/grass.png";
+import waterImg from "../../assets/water.png";
+import wallImg from "../../assets/wall.png";
+import stoneImg from "../../assets/stone.png";
+import holeImg from "../../assets/hole.png";
 
 const Home = () => {
-
   const [space, setSpace] = useState(500);
   const [grass, setGrass] = useState(30);
   const [water, setWater] = useState(30);
   const [stone, setStone] = useState(30);
   const [hole, setHole] = useState(30);
 
-  const maxImmovable = ~~ (space * 0.15);
+  const maxImmovable = ~~(space * 0.15);
 
   return (
     <div className="home-wrapper">
       <h1>MAPMAKER</h1>
       <p className="desc">
-        This is a map random generator. We attempt to use a set of rules to generate maze maps with different styles.
-        As long as you continue to explore within the known map, the map will be gradually completed.
-        Fill in the necessary parameters below to get started.
+        This is a map random generator. We attempt to use a set of rules to
+        generate maze maps with different styles. As long as you continue to
+        explore within the known map, the map will be gradually completed. Fill
+        in the necessary parameters below to get started.
       </p>
       <ul className="cfg-list">
         <li>
           <div className="img-wrapper">
-            <img src="/src/assets/earth.png" alt="Space"/>
-            <Popover placement="topRight" content="The content in space. Can be moved The quantity is 10%~15% of the space">
+            <img src={earthImg} alt="Space" />
+            <Popover
+              placement="topRight"
+              content="The content in space. Can be moved The quantity is 10%~15% of the space"
+            >
               <QuestionCircleOutlined />
             </Popover>
           </div>
 
           <h3>Space</h3>
-          <InputNumber value={space} onChange={(value) => setSpace(~~value)}/>
+          <InputNumber value={space} onChange={(value) => setSpace(~~value)} />
         </li>
         <li>
           <div className="img-wrapper">
-            <img src="/src/assets/grass.png" alt="Grass"/>
-            <Popover width={100} placement="topRight" content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space The quantity is 10%~15% of the space">
+            <img src={grassImg} alt="Grass" />
+            <Popover
+              width={100}
+              placement="topRight"
+              content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space The quantity is 10%~15% of the space"
+            >
               <QuestionCircleOutlined />
             </Popover>
           </div>
           <h3>Grass</h3>
-          <Slider value={grass} min={1} max={maxImmovable} onChange={(value) => setGrass(value)}/>
+          <Slider
+            value={grass}
+            min={1}
+            max={maxImmovable}
+            onChange={(value) => setGrass(value)}
+          />
           <div className="cfg-count">{grass}</div>
         </li>
         <li>
           <div className="img-wrapper">
-            <img src="/src/assets/water.png" alt="Sea"/>
-            <Popover placement="topRight" content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space. The quantity is 10%~15% of the space">
+            <img src={waterImg} alt="Sea" />
+            <Popover
+              placement="topRight"
+              content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space. The quantity is 10%~15% of the space"
+            >
               <QuestionCircleOutlined />
             </Popover>
           </div>
           <h3>Sea</h3>
-          <Slider value={water} min={1} max={maxImmovable} onChange={(value) => setWater(value)}/>
+          <Slider
+            value={water}
+            min={1}
+            max={maxImmovable}
+            onChange={(value) => setWater(value)}
+          />
           <div className="cfg-count">{water}</div>
         </li>
         <li>
           <div className="img-wrapper">
-            <img src="/src/assets/stone.png" alt="Stone"/>
-            <Popover placement="topRight" content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space. The quantity is 10%~15% of the space">
+            <img src={stoneImg} alt="Stone" />
+            <Popover
+              placement="topRight"
+              content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space. The quantity is 10%~15% of the space"
+            >
               <QuestionCircleOutlined />
             </Popover>
           </div>
           <h3>Stone</h3>
-          <Slider value={stone} min={1} max={maxImmovable} onChange={(value) => setStone(value)}/>
+          <Slider
+            value={stone}
+            min={1}
+            max={maxImmovable}
+            onChange={(value) => setStone(value)}
+          />
           <div className="cfg-count">{stone}</div>
         </li>
         <li>
           <div className="img-wrapper">
-            <img src="/src/assets/hole.png" alt="Hole"/>
-            <Popover placement="topRight" content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space. The quantity is 10%~15% of the space">
+            <img src={holeImg} alt="Hole" />
+            <Popover
+              placement="topRight"
+              content="The content in space. It cannot be moved. Can be destroyed, and after destruction, it becomes a space. The quantity is 10%~15% of the space"
+            >
               <QuestionCircleOutlined />
             </Popover>
           </div>
           <h3>Hole</h3>
-          <Slider value={hole} min={1} max={maxImmovable} onChange={(value) => setHole(value)}/>
+          <Slider
+            value={hole}
+            min={1}
+            max={maxImmovable}
+            onChange={(value) => setHole(value)}
+          />
           <div className="cfg-count">{hole}</div>
         </li>
       </ul>
@@ -87,9 +130,12 @@ const Home = () => {
           stone,
           water,
           grass,
-          space
+          space,
         }}
-        className="go-map">GO</Link>
+        className="go-map"
+      >
+        GO
+      </Link>
     </div>
   );
 };
--- packages/client/src/pages/map/index.tsx
+++ packages/client/src/pages/map/index.tsx
@@ -1,59 +1,79 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { useLocation, Link } from 'react-router-dom';
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { useLocation, Link } from "react-router-dom";
 import {
-  createEmptyData, expandMapData,
-  flagToClassName, getAdjacentTypeCount, getFilledWallData, getFormatMUDData, getImmovableCount,
+  createEmptyData,
+  expandMapData,
+  flagToClassName,
+  getAdjacentTypeCount,
+  getFilledWallData,
+  getFormatMUDData,
+  getImmovableCount,
   getRandomMovableType,
   getRandomType,
-  getTargetAroundPoint, isMovableType
-} from '../../utils';
-import MapCell from './MapCell';
+  getTargetAroundPoint,
+  isMovableType,
+} from "../../utils";
+import MapCell from "./MapCell";
 
-import './styles.scss';
-import { LimitSpace, MaxImmovableCount, Side, TypeFlags, Types } from '../../contants';
-import { message } from 'antd';
-import { useMUD } from '../../MUDContext';
+import "./styles.scss";
+import {
+  LimitSpace,
+  MaxImmovableCount,
+  Side,
+  TypeFlags,
+  Types,
+} from "../../contants";
+import { message } from "antd";
+import { useMUD } from "../../MUDContext";
+
+import earthImg from "../../assets/earth.png";
+import grassImg from "../../assets/grass.png";
+import waterImg from "../../assets/water.png";
+import wallImg from "../../assets/wall.png";
+import stoneImg from "../../assets/stone.png";
+import holeImg from "../../assets/hole.png";
 
 const Map = () => {
-
   const width = 43;
   const height = 23;
 
   const [vertexCoordinate, setVertexCoordinate] = useState({
     x: 0,
-    y: 0
-  })
+    y: 0,
+  });
 
   const { x: startX, y: startY } = vertexCoordinate;
 
   const [data, setData] = useState(createEmptyData(width, height));
   const [target, setTarget] = useState({
     x: ~~(width / 2),
-    y: ~~(height / 2)
-  })
+    y: ~~(height / 2),
+  });
   const cellClassCache = useRef({});
   const { state = {} } = useLocation();
   const cellTypeCount = useRef({ ...state });
   const [finished, setFinished] = useState(false);
 
   const staticData = useMemo(() => {
-    return Array(height).fill(0).map(() => Array(width).fill(0));
+    return Array(height)
+      .fill(0)
+      .map(() => Array(width).fill(0));
   }, [width, height]);
 
   const {
     systemCalls: { move },
   } = useMUD();
 
-
   const init = () => {
     data[target.y][target.x] = TypeFlags.space;
     cellTypeCount.current.space--;
     setData([...data]);
-  }
+  };
 
   const setAroundPoints = () => {
     const aroundPoints = getTargetAroundPoint(target);
-    let lastImmovableCount = MaxImmovableCount - getImmovableCount([...aroundPoints, target], data);
+    let lastImmovableCount =
+      MaxImmovableCount - getImmovableCount([...aroundPoints, target], data);
     for (const point of aroundPoints) {
       if (cellTypeCount.current?.space === 0) {
         break;
@@ -62,7 +82,10 @@ const Map = () => {
         continue;
       }
       const adjacentTypeCount = getAdjacentTypeCount(data, point);
-      const type = lastImmovableCount <= 0 ? getRandomMovableType(cellTypeCount.current, adjacentTypeCount) : getRandomType(cellTypeCount.current, adjacentTypeCount);
+      const type =
+        lastImmovableCount <= 0
+          ? getRandomMovableType(cellTypeCount.current, adjacentTypeCount)
+          : getRandomType(cellTypeCount.current, adjacentTypeCount);
       data[point.y][point.x] = TypeFlags[type];
       if (type !== Types.wall) {
         cellTypeCount.current[type]--;
@@ -73,7 +96,7 @@ const Map = () => {
       }
     }
     setData([...data]);
-  }
+  };
 
   const onKeyDown = (e) => {
     if (finished || e.keyCode < 37 || e.keyCode > 40) {
@@ -84,7 +107,7 @@ const Map = () => {
         target.x--;
         if (target.x === LimitSpace) {
           expandMapData(data, Side.left);
-          target.x++
+          target.x++;
         }
         if (target.x - vertexCoordinate.x === LimitSpace) {
           vertexCoordinate.x--;
@@ -103,14 +126,14 @@ const Map = () => {
       case 39:
         target.x++;
         if (data[0].length - target.x === LimitSpace) {
-          expandMapData(data, Side.right)
+          expandMapData(data, Side.right);
         }
         if (width - target.x + vertexCoordinate.x === LimitSpace) {
           vertexCoordinate.x++;
         }
         break;
       default:
-        target.y++
+        target.y++;
         if (data.length - target.y === LimitSpace) {
           expandMapData(data, Side.bottom);
         }
@@ -120,19 +143,19 @@ const Map = () => {
         break;
     }
     setTarget({
-      ...target
+      ...target,
     });
     setVertexCoordinate({
-      ...vertexCoordinate
+      ...vertexCoordinate,
     });
     setData([...data]);
     setAroundPoints();
-  }
+  };
 
   const save = () => {
     const { start, end, mudData } = getFormatMUDData(data);
     move(target.x - start.x, target.y - start.y, end.x - start.x, mudData);
-  }
+  };
 
   useEffect(() => {
     init();
@@ -141,79 +164,74 @@ const Map = () => {
 
   useEffect(() => {
     if (cellTypeCount.current?.space === 0) {
-      message.info('The map is generated');
+      message.info("The map is generated");
       setFinished(true);
       setData(getFilledWallData(data));
     }
-  }, [cellTypeCount.current?.space])
-
+  }, [cellTypeCount.current?.space]);
 
   return (
     <div className="mi-map-wrapper" onKeyDown={onKeyDown} tabIndex={0}>
       <div className="tool-tip">
         <ul className="cfg-list">
           <li>
-            <img src="/src/assets/earth.png" alt="Space"/>
+            <img src={earthImg} alt="Space" />
             <div className="type-count">{cellTypeCount.current.space}</div>
           </li>
           <li>
-            <img src="/src/assets/grass.png" alt="Grass"/>
+            <img src={grassImg} alt="Grass" />
             <div className="type-count">{cellTypeCount.current.grass}</div>
           </li>
           <li>
-            <img src="/src/assets/water.png" alt="Sea"/>
+            <img src={waterImg} alt="Sea" />
             <div className="type-count">{cellTypeCount.current.water}</div>
           </li>
           <li>
-            <img src="/src/assets/stone.png" alt="Stone"/>
+            <img src={stoneImg} alt="Stone" />
             <div className="type-count">{cellTypeCount.current.stone}</div>
           </li>
           <li>
-            <img src="/src/assets/hole.png" alt="Hole"/>
+            <img src={holeImg} alt="Hole" />
             <div className="type-count">{cellTypeCount.current.hole}</div>
           </li>
         </ul>
         <div className="opt-wrapper">
-          <button
-            className="save"
-            disabled={!finished}
-            onClick={save}
-          >Save</button>
-          <button className="restart" onClick={() => window.location.reload()}>Restart</button>
+          <button className="save" disabled={!finished} onClick={save}>
+            Save
+          </button>
+          <button className="restart" onClick={() => window.location.reload()}>
+            Restart
+          </button>
           <button className="back">
             <Link to="/">Back</Link>
           </button>
         </div>
       </div>
       <div className="mi-map-content">
-      {
-        staticData.map((row, rowIndex) => {
+        {staticData.map((row, rowIndex) => {
           return (
             <div className="mi-map-row" key={startY + rowIndex}>
-              {
-                row.map((_, colIndex) => {
-                  const x = startX + colIndex;
-                  const y = startY + rowIndex;
-                  const isTarget = x === target.x && y === target.y;
-                  const typeClass = flagToClassName(data[y][x]);
-                  return (
-                    <MapCell
-                      key={startX + colIndex}
-                      coordinate={{
-                        x,
-                        y
-                      }}
-                      className={`${isTarget ? 'target' : ''} ${typeClass}`}
-                      mapData={data}
-                      cellClassCache={cellClassCache}
-                    />
-                  )
-                })
-              }
+              {row.map((_, colIndex) => {
+                const x = startX + colIndex;
+                const y = startY + rowIndex;
+                const isTarget = x === target.x && y === target.y;
+                const typeClass = flagToClassName(data[y][x]);
+                return (
+                  <MapCell
+                    key={startX + colIndex}
+                    coordinate={{
+                      x,
+                      y,
+                    }}
+                    className={`${isTarget ? "target" : ""} ${typeClass}`}
+                    mapData={data}
+                    cellClassCache={cellClassCache}
+                  />
+                );
+              })}
             </div>
-          )
-        })
-      }
+          );
+        })}
       </div>
     </div>
   );
